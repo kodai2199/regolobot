b5aaa4cb507d605e2572066b48942376ac0f083e
kodai2199 <ganzerli.giulio.99@gmail.com>
2024-03-08 18:10:02 +0100

    Added Dataset Creation script

--- rapid_start.sh
+++
@@ -1,7 +0,0 @@
-cd /home/ros/ws_moveit/
-source devel/setup.bash
-export GAZEBO_MODEL_PATH=/home/ros/ws_moveit/src/regolobot/models
-roslaunch moveit_robot_arm_sim full_robot_arm_sim.launch &
-sleep 10
-rosrun regolobot spawn_model_server.py
-trap 'kill $(jobs -p)' EXIT
